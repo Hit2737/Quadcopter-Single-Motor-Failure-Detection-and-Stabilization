5b3075c73ebe6f8baa4b8de2a005b356ab42835f
Demolus13 <parth.govale@gmail.com>
2024-11-30 22:42:09 +0530

    Fix errors in motor control logic and enhance debugging output

--- src/px4_model/px4_model/motor_model.py
+++ src/px4_model/px4_model/motor_model.py
@@ -1,64 +1,30 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
-from px4_msgs.msg import SensorCombined, VehicleGlobalPosition, SensorGps
+from px4_msgs.msg import VehicleOdometry
 from std_msgs.msg import Float32MultiArray
 
 import numpy as np
+from scipy.spatial.transform import Rotation as R
+from scipy.linalg import pinv
 
-def R_a2_theta(theta):
-    return np.array([
-        [np.cos(theta), 0, np.sin(theta)],
-        [0, 1, 0],
-        [-np.sin(theta), 0, np.cos(theta)]
-    ])
+def rotate_vector_from_to_enu_ned(vec_in):
+    vec_out = np.array([vec_in[1], vec_in[0], -vec_in[2]])
+    return vec_out
 
-def R_a1_phi(phi):
-    return np.array([
-        [1, 0, 0],
-        [0, np.cos(phi), -np.sin(phi)],
-        [0, np.sin(phi), np.cos(phi)]
-    ])
+def rotate_quaternion_from_to_enu_ned(quat_in):
+    euler_1 = [np.pi, 0.0, np.pi / 2]
+    NED_ENU_Q = R.from_euler('zyx', euler_1)
 
-def R_a3_psi(psi):
-    return np.array([
-        [np.cos(psi), -np.sin(psi), 0],
-        [np.sin(psi), np.cos(psi), 0],
-        [0, 0, 1]
-    ])
+    euler_2 = [np.pi, 0.0, 0.0]
+    AIRCRAFT_BASELINK_Q = R.from_euler('zyx', euler_2)
 
-def skew_symmetric(vector):
-    return np.array([
-        [0, -vector[2], vector[1]],
-        [vector[2], 0, -vector[0]],
-        [-vector[1], vector[0], 0]
-    ])
+    transformed_quat = NED_ENU_Q * quat_in * AIRCRAFT_BASELINK_Q
+    return transformed_quat
 
-class PIDController:
-    def __init__(self, KP, KD, KI, size = 3, integral_limit = None):
-        self.KP = KP
-        self.KD = KD
-        self.KI = KI
-
-        # Initialize Errors
-        self.integral = np.zeros(size)
-        self.integral_limit = integral_limit
-
-    def update(self, error, derivative_error, dt):
-        # Update integral term
-        self.integral += error * dt
-        
-        if self.integral_limit:
-            self.integral = np.clip(self.integral, -self.integral_limit, self.integral_limit)
-
-        # PID output
-        output = self.KP * error + self.KI * self.integral + self.KD * derivative_error
-
-        return output
+def rotate_vector_from_to_frd_flu(vec_in):
+    vec_out = np.array([vec_in[0], -vec_in[1], -vec_in[2]])
+    return vec_out
 
 class ThrottlePublisher(Node):
     def __init__(self):
@@ -71,25 +37,37 @@ class ThrottlePublisher(Node):
             depth=10
         )
 
-        # PID Controllers
-        self.force_controller = PIDController(KP=np.array([0.051]), KD=np.array([2.0]), KI=np.array([0.0]), size=1)
-        self.moment_controller = PIDController(KP=np.array([0.01, 0.01, 0.01]), KD=np.array([0.0, 0.0, 0.0]), KI=np.array([0.0, 0.0, 0.0]), size=3)
-        self.attitude_controller = PIDController(KP=np.array([10.0, 10.0]), KD=np.array([6.0, 6.0]), KI=np.array([0.0, 0.0]), size=2)
-        self.yaw_controller = PIDController(KP=np.array([0.1]), KD=np.array([2.0]), KI=np.array([0.0]), size=1)
+        self.mass = 1.725
+        self.inertia_matrix = np.diag([0.029125, 0.029125, 0.055225])
+        self.gravity = 9.81
+        self.arm_length = 0.25
+        self.moment_constant = 0.06
+        self.thrust_constant = 5.84e-06
+        self.max_rotor_speed = 1100.0
 
-        # Initialize data storage
-        self.sensor_combined_data = None
-        self.vehicle_gps_position_data = None
-        self.vehicle_global_position_data = None
+        self.position = np.zeros(3)
+        self.velocity = np.zeros(3)
+        self.orientation = np.zeros(3)
+        self.angular_velocity = np.zeros(3)
+
+        self.position_r = np.array([0, 0, 0])
+        self.velocity_r = np.array([0, 0, 0])
+        self.acceleration_r = np.array([0, 0, 0])
+        self.yaw_r = 0
+        self.yaw_rate_r = 0
+
+        self.position_gain = np.array([0.5, 0.5, 0.5])
+        self.velocity_gain = np.array([0.5, 0.5, 0.5])
+        self.attitude_gain = np.array([0.5, 0.5, 0.5])
+        self.angular_rate_gain = np.array([0.5, 0.5, 0.5])
+        self.omega_to_pwn_coefficients = np.array([0.001142, 0.2273, 914.2])
+        self.pwm_min = 1075
+        self.pwm_max = 1950
+
+        self.compute_control_allocation_and_actuator_matrices()
 
         # Create subscribers with QoS settings
-        self.create_subscription(SensorCombined, '/fmu/out/sensor_combined', self.sensor_combined_callback, qos_profile)
-        self.create_subscription(SensorGps, '/fmu/out/vehicle_gps_position', self.vehicle_gps_position_callback, qos_profile)
-        self.create_subscription(VehicleGlobalPosition, '/fmu/out/vehicle_global_position', self.vehicle_global_position_callback, qos_profile)
-
-        self.base_lat = 47.397742
-        self.base_lon = 8.545594
-        self.base_alt = 488.0
+        self.create_subscription(VehicleOdometry, '/fmu/out/vehicle_odometry', self.vehicle_odometry_callback, qos_profile)
 
         self.throttle_values = [0.0, 0.0, 0.0, 0.0]
         self.dt = 0.005
@@ -97,102 +75,138 @@ class ThrottlePublisher(Node):
         self.control_timer = self.create_timer(self.dt, self.update_control_loop)
         self.throttle_timer = self.create_timer(self.dt, self.publish_throttle_values)
 
-        # Desired_state = [X, Y, Z, Vx, Vy, Vz, Ax, Ay, Az, Phi, Theta, Psi]
-        self.desired_state = [0.0, 0.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        self.errors = [np.zeros(6)]
-
-        self.mass = 1.5
-        self.J = np.diag([0.029125, 0.029125, 0.055225])
-        self.g = 9.81
-        self.max_motor_speed = 1100
-
-        # Motor Parameters
-        FL_b1 = 0.13
-        BL_b1 = 0.13
-        FR_b1 = 0.13
-        BR_b1 = 0.13
-
-        FR_b2 = 0.22
-        BL_b2 = 0.2
-        BR_b2 = 0.2
-        FL_b2 = 0.22
-
-        KF = 5.84e-06
-        KM = 0.06
-        self.allocation_matrix = np.array([
-            [KF, KF, KF, KF],
-            [-KF * FR_b2, KF * BL_b2, KF * FL_b2, -KF * BR_b2],
-            [-KF * FR_b1, KF * BL_b1, -KF * FL_b1, KF * BR_b1],
-            [KM, KM, -KM, -KM]
-        ])
-
     def publish_throttle_values(self):
         msg = Float32MultiArray()
         msg.data = [self.throttle_values[0], self.throttle_values[1], self.throttle_values[2], self.throttle_values[3]]
-        # msg.data = [0.5, 0.5, 0.5, 0.5]
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Published throttle values: {msg.data}')
 
-    def sensor_combined_callback(self, msg):
-        self.sensor_combined_data = [msg.timestamp] + list(msg.gyro_rad) + list(msg.accelerometer_m_s2)
+    def compute_control_allocation_and_actuator_matrices(self):
+        k_deg_to_rad = np.pi / 180.0
+        k_s = np.sin(45.0 * k_deg_to_rad)
 
-    def vehicle_gps_position_callback(self, msg):
-        self.vehicle_gps_position_data = [msg.timestamp, msg.latitude_deg, msg.longitude_deg, msg.altitude_msl_m, msg.vel_m_s, msg.vel_n_m_s, msg.vel_e_m_s, msg.vel_d_m_s]
+        self.rotor_velocities_to_torques_and_thrust = np.array([
+            [-k_s,  k_s,  k_s, -k_s],
+            [-k_s,  k_s, -k_s,  k_s],
+            [-1,   -1,    1,    1],
+            [ 1,    1,    1,    1]
+        ])
+
+        self.mixing_matrix = np.array([
+            [-0.495384,  -0.707107,  -0.765306,   1.0],
+            [ 0.495384,   0.707107,  -1.0,        1.0],
+            [ 0.495384,  -0.707107,   0.765306,   1.0],
+            [-0.495384,   0.707107,   1.0,        1.0]
+        ])
+
+        self.throttles_to_normalized_torques_and_thrust = np.array([
+            [-0.5718,    0.4376,    0.5718,   -0.4376],
+            [-0.3536,    0.3536,   -0.3536,    0.3536],
+            [-0.2832,   -0.2832,    0.2832,    0.2832],
+            [ 0.2500,    0.2500,    0.2500,    0.2500]
+        ])
+
+        k = np.diag([
+            self.thrust_constant * self.arm_length, 
+            self.thrust_constant * self.arm_length,
+            self.moment_constant * self.thrust_constant, 
+            self.thrust_constant
+        ])
+
+        self.rotor_velocities_to_torques_and_thrust = k @ self.rotor_velocities_to_torques_and_thrust
+        self.torques_and_thrust_to_rotor_velocities = pinv(self.rotor_velocities_to_torques_and_thrust)
+
+        print("rotor_velocities_to_torques_and_thrust = \n", self.rotor_velocities_to_torques_and_thrust)
+        print("torques_and_thrust_to_rotor_velocities = \n", self.torques_and_thrust_to_rotor_velocities)
+        print("throttles_to_normalized_torques_and_thrust_ = \n", self.throttles_to_normalized_torques_and_thrust)
+
+    def eigen_odometry_from_px4_msg(self, msg):
+        position_W = rotate_vector_from_to_enu_ned(np.array(msg["position"]))
+        quaternion = R.from_quat([msg["q"][0], msg["q"][1], msg["q"][2], msg["q"][3]])
+        orientation_B_W = rotate_quaternion_from_to_enu_ned(quaternion)
+        velocity_B = rotate_vector_from_to_enu_ned(np.array(msg["velocity"]))
+        angular_velocity_B = rotate_vector_from_to_frd_flu(np.array(msg["angular_velocity"]))
+
+        return position_W, orientation_B_W, velocity_B, angular_velocity_B
+
+    def vehicle_odometry_callback(self, msg):
+        position_W, orientation_B_W, velocity_B, angular_velocity_B = self.eigen_odometry_from_px4_msg(msg)
+        self.R_B_W = orientation_B_W.as_matrix()
+        self.position = position_W
+        self.velocity = self.R_B_W.T @ velocity_B
+        self.angular_velocity = angular_velocity_B
     
-    def vehicle_global_position_callback(self, msg):
-        self.vehicle_global_position_data = [msg.timestamp, msg.lat-self.base_lat, msg.lon-self.base_lon, msg.alt-self.base_alt]
+    def calculate_controller_output(self, desired_quaternion):
+        # Compute translational tracking errors
+        e_p = self.position - self.position_r
+        e_v = self.velocity - self.velocity_r
 
-    def compute_body_frame_matrix(self, phi, theta, psi):
-        return np.dot(R_a3_psi(psi), R_a1_phi(phi), R_a2_theta(theta))
+        I_a_d = (
+            -np.multiply(self.position_gain, e_p)
+            - np.multiply(self.velocity_gain, e_v)
+            + self.mass * self.gravity * np.array([0, 0, 1])
+            + self.mass * self.acceleration_r
+        )
+        thrust = I_a_d.dot(self.R_B_W[:, 2])
+        B_z_d = I_a_d / np.linalg.norm(I_a_d)
 
-    def compute_motor_speeds(self, F, M):
-        omega_squared = np.linalg.inv(self.allocation_matrix) @ np.array([F, M[0], M[1], M[2]])
-        self.omega = np.sqrt(np.clip(omega_squared, 0, self.max_motor_speed**2))
-        self.omega = self.omega / self.max_motor_speed
-        return self.omega
+        # Calculate desired rotation matrix
+        B_x_d = np.array([np.cos(self.yaw_r), np.sin(self.yaw_r), 0.0])
+        B_y_d = np.cross(B_z_d, B_x_d)
+        B_y_d /= np.linalg.norm(B_y_d)
+
+        R_d_w = np.zeros((3, 3))
+        R_d_w[:, 0] = np.cross(B_y_d, B_z_d)
+        R_d_w[:, 1] = B_y_d
+        R_d_w[:, 2] = B_z_d
+
+        # Compute desired quaternion
+        desired_quaternion_temp = R.from_matrix(R_d_w).as_quat()  # Quaternion as [x, y, z, w]
+        desired_quaternion[:] = desired_quaternion_temp
+
+        # Attitude tracking
+        e_R_matrix = 0.5 * (R_d_w.T @ self.R_B_W - self.R_B_W.T @ R_d_w)
+        e_R = np.array([e_R_matrix[2, 1], e_R_matrix[0, 2], e_R_matrix[1, 0]])
+        omega_ref = self.yaw_rate_r * np.array([0, 0, 1])
+        e_omega = self.angular_velocity - self.R_B_W.T @ R_d_w @ omega_ref
+
+        tau = (
+            -np.multiply(self.attitude_gain, e_R)
+            - np.multiply(self.angular_rate_gain, e_omega)
+            + np.cross(
+                self.angular_velocity,
+                np.dot(np.diag(self.inertia_matrix), self.angular_velocity)
+            )
+        )
+
+        # Output the wrench
+        wrench = np.zeros(4)
+        wrench[:3] = tau
+        wrench[3] = thrust
+        
+        return wrench
+    
+    def inverse_kinematics(self, wrench):
+        omega = self.torques_and_thrust_to_rotor_velocities @ wrench
+        omega = np.sqrt(np.clip(omega, 0, self.max_rotor_speed))
+
+        ones_temp = np.ones(4)
+        pwm = (
+            self.omega_to_pwn_coefficients[0] * omega**2
+            + self.omega_to_pwn_coefficients[1] * omega
+            + self.omega_to_pwn_coefficients[2] * ones_temp
+        )
+
+        throttles = (pwm - self.pwm_min * ones_temp) / (self.pwm_max - self.pwm_min)
+
+        normalized_throttles = self.throttles_to_normalized_torques_and_thrust @ throttles
+        return normalized_throttles, throttles
     
     def update_control_loop(self):
-        if self.sensor_combined_data and self.vehicle_global_position_data:
-            current_state = [0] * 6
+        
+        wrench = self.calculate_controller_outputs()
+        normalized_throttles, throttles = self.inverse_kinematics(wrench)
 
-            current_state[:3] = self.vehicle_global_position_data[1:4]
-            current_state[3:6] = self.sensor_combined_data[1:4]
-
-            print(f"Current State: {current_state}")
-
-            BFM = self.compute_body_frame_matrix(current_state[3], current_state[4], current_state[5])
-            R_body = BFM[:3, :3]
-
-            self.errors.append(np.zeros(6))
-            if len(self.errors) > 2:
-                self.errors.pop(0)
-            self.errors[-1][:3] = np.dot(R_body, np.array(self.desired_state[:3]) - np.array(current_state[:3]))
-            d_pos_error = (self.errors[-1][:3] - self.errors[-2][:3]) / self.dt
-
-            desired_attitude = np.zeros(3)
-
-            desired_attitude[:2] = self.attitude_controller.update(self.errors[-1][:2], d_pos_error[:2], self.dt)
-            desired_phi = (-1/self.g) * (self.desired_state[7] + desired_attitude[1])
-            desired_theta = (1/self.g) * (self.desired_state[6] + desired_attitude[0])
-            desired_attitude[0], desired_attitude[1]  = desired_phi, desired_theta
-            desired_attitude[2] = self.yaw_controller.update(np.array([0]), np.array([0]), self.dt)[0]
-
-            self.errors[-1][3:6] = desired_attitude - np.dot(R_body, current_state[3:6])
-            d_angle_error = (self.errors[-1][3:6] - self.errors[-2][3:6]) / self.dt
-
-            acc = self.force_controller.update(np.array([self.errors[-1][2]]), np.array([d_pos_error[2]]), self.dt)
-            force = self.mass * (self.g + self.desired_state[8] + acc / (np.cos(current_state[3]) * np.cos(current_state[4])))
-            moment = self.moment_controller.update(self.errors[-1][3:6], d_angle_error, self.dt)
-            print("--------------------")
-            print(f"Desired Attitude: {desired_attitude}, Current Attitude: {np.dot(R_body, current_state[3:6])}")
-            print("====================================")
-            print("Errors: ", self.errors[-1][3:6])
-            motor_speeds = self.compute_motor_speeds(force[0], moment)
-
-            print(f"Force: {force[0]}, Moment: {moment}")
-            # print(f"Errors: {self.errors[-1]}")
-
-            self.throttle_values = motor_speeds
+        self.throttle_values = throttles
 
 def main(args=None):
     rclpy.init(args=args)
